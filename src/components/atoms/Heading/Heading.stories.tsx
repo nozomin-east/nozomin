8554b6b38264333639a4624edcbae7e406d1628b
Kashida <nkashida@coiney.com>
2019-03-25 07:45:21 +0900

    update story

--- src/components/atoms/Heading/Heading.stories.tsx
+++ src/components/atoms/Heading/Heading.stories.tsx
@@ -3,8 +3,8 @@ import { withInfo } from '@storybook/addon-info';
 import { storiesOf } from '@storybook/react';
 import Heading from './';
 
-storiesOf('/Heading', module)
-  .add('Heading', withInfo()(() => (
+storiesOf('/Atoms/Heading', module)
+  .add('Size', withInfo()(() => (
     <React.Fragment>
       <h2>h1～h6</h2>
       <Heading as="h1">Heading</Heading>
@@ -13,5 +13,11 @@ storiesOf('/Heading', module)
       <Heading as="h4">Heading</Heading>
       <Heading as="h5">Heading</Heading>
       <Heading as="h6">Heading</Heading>
+    </React.Fragment>
+  )))
+  .add('Center', withInfo()(() => (
+    <React.Fragment>
+      <h2>Center</h2>
+      <Heading as="h1" center>Heading</Heading>
     </React.Fragment>
   )));
