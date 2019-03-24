93831c92a7cd398931daa7000df6068a38eaa0e7
Kashida <nkashida@coiney.com>
2019-03-24 18:35:14 +0900

    FIXMEコメント

--- .storybook/Container.tsx
+++ .storybook/Container.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
-import styles from '../src/themes/theme.scss';
+import styles from '../src/themes/theme.scss'; // FIXME ts2307
 
 interface ContainerProps {
   children: React.ReactNode;
